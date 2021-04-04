3f8e8af7268b94ea5c81769617fc5d1e8f81cffc
daniel <abdavl@almbrand.dk>
2021-04-04 21:21:56 +0200

    add terraform backend provisioning files

---
+++ terraform-backend/variables.tf
@@ -0,0 +1,31 @@
+variable "sa_resource_group_name" {
+
+}
+
+variable "subscription_id" {
+
+}
+
+variable "backend_client_id" {
+
+}
+
+variable "backend_client_secret" {
+
+}
+
+variable "tenant_id" {
+
+}
+
+variable "location" {
+    default = "westeurope"
+}
+
+variable "project_id" {
+
+}
+
+variable "storage_account_name" {
+  
+}  
